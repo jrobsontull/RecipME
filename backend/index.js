2cc329c0f03fb480c4d10d326e83e626ccfd22e2
galacticfan <jakerobsontull@gmail.com>
2021-12-31 11:41:49 -0500

    Begin backend work

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTRECIPES_DB_URI,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+    }
+).catch(err => {
+    console.log("Database connection error")
+    console.error(err.stack)
+    process.exit(1)
+}).then(async client => {
+    app.listen(port, () => {
+        console.log('Listening on port ' + port)
+    })
+})
