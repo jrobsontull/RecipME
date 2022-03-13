3c5009335a0106e4ee44201888e77122b593f24e
galacticfan <jakerobsontull@gmail.com>
2022-03-12 19:19:29 -0500

    More work on search page

--- frontend/src/components/recipe-list-extended.js
+++ frontend/src/components/recipe-list-extended.js
@@ -2,26 +2,46 @@ import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../utils/auth.context';
 
+import Logo from '../assets/img/pie_logo_orange.svg';
+
 function RecipeListExtended() {
   const { user } = useContext(AuthContext);
   const [recipesSearch, setRecipesSearch] = useState([]);
 
   return (
     <div className="react-container">
-      <div className="name">
+      <div className="list-extended-name">
         <Link to={'/my-recipes'}>
           <div className="arrow left" />
         </Link>
         <h3>Search param name here</h3>
       </div>
-      <div className="list-box">
+      <div className="list-box extended">
         <ul>
           <li>1</li>
           <li>2</li>
           <li>3</li>
           <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
         </ul>
       </div>
+      <div className="page-nav">
+        <div className="arrow-btn">
+          <div className="arrow left"></div>
+        </div>
+        <div className="current-page">1</div>
+        <div className="out-pages">out of 2</div>
+        <div className="arrow-btn">
+          <div className="arrow right"></div>
+        </div>
+      </div>
+      <img className="pie-logo" src={Logo} alt="logo" />
+      <div className="line-br"></div>
     </div>
   );
 }
